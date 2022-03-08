7f86098687a5c34a35de44e5047a6c8052b138b8
thehambeard <61787444+thehambeard@users.noreply.github.com>
2022-03-07 20:30:32 -0500

    fixed loading issue with 1.2.0.af

--- UMMUnityExplorer/Main.cs
+++ UMMUnityExplorer/Main.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using UnityExplorer;
 using UnityModManagerNet;
+using HarmonyLib;
+using Kingmaker;
 
 namespace UMMUnityExplorer
 {
@@ -32,27 +34,34 @@ namespace UMMUnityExplorer
 		}
 
 		internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
-
+		internal static Harmony harmony;
 		internal static bool Load(UnityModManager.ModEntry modEntry)
 		{
-			try
-			{
-				Main.Logger = modEntry.Logger;
-				ExplorerStandalone.CreateInstance();
-				modEntry.OnUpdate = OnUpdate;
-
-			}
-			catch (Exception ex)
-			{
-				Main.Error(ex);
-				throw;
-			}
+			
+			Main.Logger = modEntry.Logger;
+			modEntry.OnUpdate = OnUpdate;
+			harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll();
 			return true;
 		}
 
 		public static void OnUpdate(UnityModManager.ModEntry modEntry, float f)
         {
             UnityEngine.Debug.developerConsoleVisible = false;
+		}
+
+		[HarmonyPatch]
+		public static class ContentInjector
+		{
+
+			[HarmonyPatch(typeof(MainMenu), "Awake"), HarmonyPostfix]
+			public static void MainMenu_Awake()
+			{
+				ExplorerStandalone.CreateInstance((msg, logType) =>
+				{
+					Main.Logger.Error($"{logType}: {msg}");
+				});
+			}
 		}
 	}
 }
